4b11f2ce7c9fe48bb0ef0e47f2dafcc2077e283e
GeckoEidechse <gecko.eidechse+git@pm.me>
2024-02-10 18:04:11 +0100

    feat: Add CLI application

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use remove_markdown_links::remove_markdown_links;
+
+/// Print CLI usage
+fn print_usage() {
+    println!("Expected exactly one argument");
+    println!("Usage: remove_markdown_links [String]");
+}
+
 fn main() {
-    println!("Hello, world!");
+    // Call the function from the library
+    let args: Vec<String> = std::env::args().collect();
+
+    match args.len() {
+        2 => println!("{}", remove_markdown_links(&args[1])),
+        _ => {
+            println!("Default");
+            print_usage();
+        }
+    };
 }
